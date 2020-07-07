66db29aefcdf62fc9b0e1659a6d5498333fbd07f
Vladymir <pustovetov73@gmail.com>
2020-07-07 15:42:09 +0300

    java sript slaid strelka

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+       speed: 1200,
+       adaptiveHeight: true,
+       prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png"></button>',
+       nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png"></button>'
     });
   });
