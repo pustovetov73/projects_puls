0105c544561ae9b8013691c77f273d8de636ac21
Vladymir <pustovetov73@gmail.com>
2020-07-08 16:20:03 +0300

    52 urok zakonchen

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
        speed: 1200,
        adaptiveHeight: true,
-       prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png"></button>',
-       nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png"></button>'
+       prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.png"></button>',
+       nextArrow: '<button type="button" class="slick-next"><img src="icon/right.png"></button>',
+       responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows: false
+            }
+          }
+       ]
     });
   });
