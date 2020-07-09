598d26c3fc2135e6f0a4d2221e99ecd706eb23ca
Vladymir <pustovetov73@gmail.com>
2020-07-09 17:55:11 +0300

    53 urok zakonchen

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-       speed: 1200,
-       adaptiveHeight: true,
-       prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.png"></button>',
-       nextArrow: '<button type="button" class="slick-next"><img src="icon/right.png"></button>',
-       responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-               dots: true,
-               arrows: false
-            }
-          }
-       ]
-    });
-  });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//        speed: 1200,
+//        adaptiveHeight: true,
+//        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.png"></button>',
+//        nextArrow: '<button type="button" class="slick-next"><img src="icon/right.png"></button>',
+//        responsive: [
+//           {
+//             breakpoint: 992,
+//             settings: {
+//                dots: true,
+//                arrows: false
+//             }
+//           }
+//        ]
+//     });
+//   });
+
+
+var slider = tns({
+   container: '.carousel__inner',
+   items: 1,
+   slideBy: 'page',
+   autoplay: false,
+ });
