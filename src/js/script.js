05e6eaddfae72e888a7658802580e7deaf04e2aa
Vladymir <pustovetov73@gmail.com>
2020-07-10 14:46:34 +0300

    nachalo 54uroka

--- src/js/script.js
+++ src/js/script.js
@@ -23,10 +23,15 @@ const slider = tns({
    items: 1,
    slideBy: 'page',
    autoplay: false,
+   controls: false,
+   nav: false,
  });
 
- document.querySelector('.prev').onclick = function () {
+ document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
-  };
+  });
 
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
 
